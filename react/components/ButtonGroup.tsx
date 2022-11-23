412022a5a72b3ae601f56c7f3661ba0be653c8fb
Fernando Pachon <josep3194@gmail.com>
2022-11-23 15:32:01 -0500

    feat:update add to cart

---
+++ react/components/ButtonGroup.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+const ButtonGroup = ()=>{
+  return(
+    <>
+      <div>
+        <a href="/" >Check Out</a>
+      </div>
+      <div>
+        <button>Continua Comprando</button>
+        <a href="/"> Ver carrito</a>
+      </div>
+    </>
+  )
+}
+export default ButtonGroup
